57e02061d151bc5ffdc60a78e821648d2a48658b
Luiz Fernando Azevedo <luiz1337@gmail.com>
2025-05-20 10:54:28 -0300

    project

---
+++ fnpers/src/main/java/com/galdhra/fnpers/FnpersApplication.java
@@ -0,0 +1,13 @@
+package com.galdhra.fnpers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnpersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnpersApplication.class, args);
+	}
+
+}
---
+++ fnpers/src/test/java/com/galdhra/fnpers/FnpersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galdhra.fnpers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnpersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
